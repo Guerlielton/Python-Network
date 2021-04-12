b7d3524eb3bf8c75a40f624679883b5324776c4c
Guerlielton <guerlielton@gmail.com>
2021-04-12 12:39:00 -0300

    Update ecs.tf

--- terraform-network/ecs.tf
+++ terraform-network/ecs.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_cluster" "telegram" {
     name = "bot-telegram"
 }
 
-resource "aws_ecs_task_definition" "app" {
+resource "aws_ecs_task_definition" "Bot" {
     family = "bot-task"
     execution_role_arn = "aws_iam_role.ecs_task_execution_role.arn"
     network_mode = "awsvpc"
