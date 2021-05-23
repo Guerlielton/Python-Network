813ec6a03bea4b235613e1f6cd8220e00e45e371
Guerlielton <guerlielton@gmail.com>
2021-05-23 10:04:12 -0300

    Fix: Huawei CLI

--- Telegram-bot/networking/huawei.py
+++ Telegram-bot/networking/huawei.py
@@ -3,6 +3,7 @@ def huawei_policy():
     arq = open('Huawei-Route-Policy.txt','w')
     count = 0
     v4 = 0
+    v6 = 0
     no = 100
     file = open('asn-list.txt', 'r')
     t = []
@@ -13,6 +14,8 @@ def huawei_policy():
            if 'AS' in t[i]:   
                 count = count + 1
                 no = no + 10 
+                v4 = v4 + 10
+                v6 = v6 + 10
                 k = 1
                 ipv4 = []
                 ipv6 = []
@@ -22,21 +25,28 @@ def huawei_policy():
                     else:
                         ipv4.append(i+k)
                     k = k + 1
-                for l in range(len(ipv4)):
-                    print(f'ip ip-prefix PL-{t[i]}-V4 index {v4} permit {t[ipv4[l]]}  24\n',file=arq) 
-                    v4 = v4 + 5
-                print(f'route-policy eBGP-DOWNSTREAM-ASN-{t[i]}-V4-IN permit node {no}',file=arq)
+                for l in range(len(ipv4)): 
+                    subnet = (t[ipv4[l]])[len(t[ipv4[l]])-2:]
+                    if (subnet == '24'):
+                        print(f'ip ip-prefix PL-{t[i]}-V4 index 10 permit {t[ipv4[l]].replace("/"," ")} \n',file=arq) #Remove "/" for huawei cli 
+                    else:
+                        print(f'ip ip-prefix PL-{t[i]}-V4 index 10 permit {t[ipv4[l]].replace("/"," ")} greater-equal {subnet} less-equal 24\n',file=arq) #Remove "/" for huawei cli 
+                        
+                print(f'route-policy eBGP-DOWNSTREAM-ASN-53019-AJU-IN permit node {no}',file=arq)
                 print(f'if-match ip-prefix PL-{t[i]}-V4',file=arq)
                 print('apply local-preference 700',file=arq)
-                print('apply community 1234:200 1234:243 1234:1023 1234:1043 additive \n',file=arq)
-                print('------------------------------------------------------------------------------------------------',file=arq)
+                print('apply community 53087:200 53087:298 additive',file=arq)
+                print('\n',file=arq)
                 for p in range(len(ipv6)):
-                    print(f'ip ip-prefix PL-{t[i]}-V6 index {v4} permit {t[ipv6[p]]}  48',file=arq) 
-                    v4 = v4 + 5
-                    print(f'route-policy eBGP-DOWNSTREAM-ASN-{t[i]}-V6-IN permit node {no}',file=arq)
-                    print(f'if-match ip-prefix PL-{t[i]}-V6',file=arq)
+                    subnet = (t[ipv6[p]])[len(t[ipv6[p]])-2:]
+                    if (subnet == '48'):
+                        print(f'ip ipv6-prefix PL-{t[i]}-V6 index 10 permit {t[ipv6[p]].replace("/"," ")} \n',file=arq) #Remove "/" for huawei cli
+                    else:
+                        print(f'ip ipv6-prefix PL-{t[i]}-V6 index 10 permit {t[ipv6[p]].replace("/"," ")} greater-equal {subnet} less-equal 48\n',file=arq) #Remove "/" for huawei cli
+                    print(f'route-policy eBGP-DOWNSTREAM-ASN-53019-AJU-V6-IN permit node {no}',file=arq)
+                    print(f'if-match ipv6 address prefix-list PL-{t[i]}-V6',file=arq)
                     print('apply local-preference 700',file=arq)
-                    print('apply community 1234:200 1234:243 1234:1023 1234:1043 additive \n',file=arq)
+                    print('apply community 53087:200 53087:298 additive \n',file=arq)
                     print('------------------------------------------------------------------------------------------------',file=arq)
     print('Huawei VRP')
     print('Have been configured',count,'Route Policy')
